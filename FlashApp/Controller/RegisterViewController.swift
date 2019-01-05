c911ab9d38ad62377b67b46bd4caba703d11082a
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro-2.local>
2019-01-04 18:22:35 -0600

    Registration completed

---
+++ FlashApp/Controller/RegisterViewController.swift
@@ -0,0 +1,64 @@
+//
+//  RegisterViewController.swift
+//  FlashApp
+//
+//  Created by Daniel Garofalo on 1/4/19.
+//  Copyright © 2019 Daniel Garofalo. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    
+    
+    //MARK:- Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    
+    //MARK:- Properties
+    
+    
+    //MARK:- Load up Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    //MARK:- Buttons
+    @IBAction func registerUserWhenPressed(_ sender: UIButton) {
+        
+        guard let typedEmail = emailTextField.text else {return}
+        guard let typedPassword = passwordTextField.text else {return}
+        
+        
+        //Authentication with Firebase
+        //On the complation block, we want to get back the user authentication info and the error code, hence the markup
+        Auth.auth().createUser(withEmail: typedEmail, password: typedPassword) { (user, error) in
+            
+            if error == nil {
+                print("Registration Successful!")
+                self.performSegue(withIdentifier: "goToChat", sender: nil)
+            } else {
+                print(error as Any)
+            }
+            
+            
+        }
+        
+        
+    }
+    
+    
+    //MARK:- Custom Functions
+    
+    
+    
+
+}
