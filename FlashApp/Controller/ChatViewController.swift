6661e6625122c2560d60b8754d3129e7661de04a
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro-2.local>
2019-01-14 09:12:40 -0600

    Messages sent / Animation to keyboard complete

--- FlashApp/Controller/ChatViewController.swift
+++ FlashApp/Controller/ChatViewController.swift
@@ -9,19 +9,15 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
-    
 
     //MARK:- Outlets
-    
+    @IBOutlet weak var heightConstraint : NSLayoutConstraint!
     @IBOutlet weak var messageTableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
     
-    
-    
-    //MARK:- Properties
     
     //MARK:- Load up functions
     override func viewDidLoad() {
@@ -31,12 +27,18 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.delegate = self
         messageTableView.dataSource = self
         
+        //Text field delegate
+        messageTextField.delegate = self
+        
         //XIB registration, with registration of the cell and creation of the UNib file
         messageTableView.register(UINib(nibName: "CustomMessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
         //Call the resizing of the cell
         configureTableView()
         
+        //Tap gesture to hide the keyboard. This one needs to be registered manually
+        hidingKeyboard()
+
     }
     
 
@@ -50,8 +52,36 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         catch {
             print("Problems signing out")
         }
+    }
+    
+    
+    @IBAction func sendMessagesWhenPressed(_ sender: UIButton) {
+        
+        messageTextField.endEditing(true)
+        messageTextField.isEnabled = false
+        sendButton.isEnabled = false
+        
+        let messagesDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["Sender" : Auth.auth().currentUser?.email, "MessageBody": messageTextField.text!]
+        
+        //Creates a random key for the message and saves the message dictionary on the message DB
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error, reference) in
+            
+            if error == nil {
+                print("Message saved successfully!")
+                self.messageTextField.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextField.text = ""
+                
+            } else {
+                print(error as Any)
+            }
+        }
+        
         
     }
+    
     
     //MARK:- TableView Functions
     //Number of Rows
@@ -67,13 +97,43 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
-
     //Adapt height of the cell
     func configureTableView() {
         messageTableView.rowHeight = UITableView.automaticDimension
         messageTableView.estimatedRowHeight = 500
     }
 
+    
+    
+    //MARK:- Custom Functions / Keyboard animation
+    //Shows up the keyboard (configuration)
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+        UIView.animate(withDuration: 0.15) {
+            self.heightConstraint.constant = 362 //Current measure was adjusted to fix iphone X
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //Hides the keyboard (configuration)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.15) {
+            self.heightConstraint.constant = 50 //Current measure was adjusted to fix iphone X
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //Manual function to hide the keyboard
+    func hidingKeyboard(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
+    }
+    
+    //Subfunction that calls the functionality to hide the keyboard
+    @objc func tableViewTapped(){
+        messageTextField.endEditing(true)
+    }
     
     
 }
---
+++ FlashApp/Model/Message.swift
@@ -0,0 +1,23 @@
+//
+//  Message.swift
+//  FlashApp
+//
+//  Created by Daniel Garofalo on 1/11/19.
+//  Copyright © 2019 Daniel Garofalo. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+    //MARK:- Properties
+    private(set) public var messageSender : String = ""
+    private(set) public var messageBody : String = ""
+    
+    
+    
+    
+    
+    
+    
+}
